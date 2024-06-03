01f149a320e06e3a9784deb0879c1ed2ac2da686
Jae <giffordjacob0@gmail.com>
2024-06-03 08:52:42 -0700

    finished generic template

--- src/World/Environment.js
+++ src/World/Environment.js
@@ -11,54 +11,11 @@ export default class Environment {
     if (this.debug.active) {
       this.debugFolder = this.debug.ui.addFolder('environment');
     }
-    this.setSunLight();
     this.setEnvironmentMap();
-  }
-
-  setSunLight() {
-    this.sunLight = new THREE.DirectionalLight('#ffffff', 4);
-    this.sunLight.castShadow = true;
-    this.sunLight.shadow.camera.far = 15;
-    this.sunLight.shadow.mapSize.set(1024, 1024);
-    this.sunLight.shadow.normalBias = 0.05;
-    this.sunLight.position.set(3.5, 2, -1.25);
-    this.scene.add(this.sunLight);
-
-    // Debug
-    if (this.debug.active) {
-      this.debugFolder
-        .add(this.sunLight, 'intensity')
-        .name('sunLightIntensity')
-        .min(0)
-        .max(10)
-        .step(0.001);
-
-      this.debugFolder
-        .add(this.sunLight.position, 'x')
-        .name('sunLightX')
-        .min(-5)
-        .max(5)
-        .step(0.001);
-
-      this.debugFolder
-        .add(this.sunLight.position, 'y')
-        .name('sunLightY')
-        .min(-5)
-        .max(5)
-        .step(0.001);
-
-      this.debugFolder
-        .add(this.sunLight.position, 'z')
-        .name('sunLightZ')
-        .min(-5)
-        .max(5)
-        .step(0.001);
-    }
   }
 
   setEnvironmentMap() {
     this.environmentMap = {};
-    this.environmentMap.intensity = 0.4;
     this.environmentMap.texture = this.resources.items.environmentMapTexture;
     this.environmentMap.texture.colorSpace = THREE.SRGBColorSpace;
     this.scene.environment = this.environmentMap.texture;
---
+++ src/World/Objects/Template_Model.js
@@ -0,0 +1,35 @@
+import Experience from '../Experience/Experience';
+import * as THREE from 'three';
+
+export default class Template_Model {
+  constructor() {
+    this.experience = new Experience();
+    this.scene = this.experience.scene;
+    this.resources = this.experience.resources;
+    this.time = this.experience.time;
+    this.debug = this.experience.debug;
+    // debug
+
+    // setup from gltf
+    this.resource = this.resources.items.foxModel;
+
+    // initialization functions
+
+    this.setModel();
+  }
+  setModel() {
+    this.model = this.resource.scene;
+    this.scene.add(this.model);
+
+    // shadows? uncomment
+    /*     this.model.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        child.castShadow = true;
+      }
+    }); */
+  }
+
+  update() {
+    this.animation.mixer.update(this.time.delta / 1000); // ms to s
+  }
+}
